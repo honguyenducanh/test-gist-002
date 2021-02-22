18cdf9389bf66c8683c4fd444d79c7f2bea731f0
honguyenducanh <79430306+honguyenducanh@users.noreply.github.com>
2021-02-22 14:25:40 +0700

    Update GitCoBan.java

--- src/com/laptrinhb2a/git/GitCoBan.java
+++ src/com/laptrinhb2a/git/GitCoBan.java
@@ -4,5 +4,6 @@ class GitCoBan {
 public static void main(String[]args){
         System.out.println("GitCoban");
         System.out.println("Git không cơ bản ");
+        // update heello words
         }
 }
